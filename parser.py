7881b6133407bf39fb2e43bb402a1bb8f6e3d49f
Alexey Tretyakov <48726736+ATreTCM@users.noreply.github.com>
2023-08-18 18:19:05 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,31 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import *
+from urllib.request import urlretrieve
 
+URL = 'https://sharik.ua/catalog/folgirovannye_shary/?PAGEN_1=9'
 
+def download_images(url, name):
+    try:
+        urlretrieve(url, name)
+        print(f'{name} downloaded successfully')
+    except Exception as e:
+        print(f'Error downloading {name}: {e}')
 
 def collect_data():
-
-    response = requests.get(url = 'https://sharik.ua/catalog/folgirovannye_shary/?PAGEN_1=9')
+    response = requests.get(url=URL)
     soup = BeautifulSoup(response.text, 'lxml')
-    ballons_img = soup.find_all('div', class_="one-item-thumb-ins")
-  
+    ballons_img = soup.find_all('div', class_='one-item-thumb-ins')
 
     for ballon in ballons_img:
-        
-        f = ballon.find('img')
-        url = str(f['src'])
-        name = str(f['alt'][11:])
-        try:
-            urlretrieve('https://sharik.ua/'+url,name)
-            print(name,'ok')
-        except:
-            continue
-       
+        img = ballon.find('img')
+        url = img['src']
+        name = img['alt'][11:]
+        download_images('https://sharik.ua/' + url, name)
 
 def main():
     collect_data()
-
 
 if __name__ == '__main__':
     main()
